933b65aaf74f22e979e967383c12314348fe7918
jc <emclab2011@gmail.com>
2013-11-08 12:54:49 -0600

    JC: added 2 datepickers.

--- app/assets/javascripts/template_taskx/tasks.js
+++ app/assets/javascripts/template_taskx/tasks.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+   $("#task_start_date").datepicker({dateFormat: 'yy-mm-dd'});
+});
+$(function() {
+   $("#task_finish_date").datepicker({dateFormat: 'yy-mm-dd'});
+});
